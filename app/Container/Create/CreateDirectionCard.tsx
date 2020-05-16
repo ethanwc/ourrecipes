c3eb60d43fa7e2a9c3aec98d660e0d1af39fc36d
ethanwc <ethanwc@u.washington.edu>
2020-05-15 22:47:15 -0700

    spacing fix

--- app/Container/Create/CreateDirectionCard.tsx
+++ app/Container/Create/CreateDirectionCard.tsx
@@ -1,66 +1,87 @@
-import 'react-native-gesture-handler'
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
-import { Theme, Typography } from '../../assets/styles';
-import { Input, Icon } from 'react-native-elements';
-import VoiceWrapper from '../../utils/VoiceWrapper/VoiceWrapper';
-import wordsToNumbers from 'words-to-numbers';
+import 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Theme, Typography} from '../../assets/styles';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
-import { TextInput } from 'react-native-gesture-handler';
+import {TextInput} from 'react-native-gesture-handler';
+import {Text, Input, Icon} from 'react-native-elements';
+import {
+  TextField,
+  FilledTextField,
+  OutlinedTextField,
+} from 'react-native-material-textfield';
 
 var parser = require('ingredients-parser');
 
 export interface createDirectionCardProps {
-    // amount: Number;
-    // ingredient: string;
-    // unit: string;
+  // amount: Number;
+  // ingredient: string;
+  // unit: string;
 }
 
 const CreateDirectionCard = (props: createDirectionCardProps) => {
-    const [input, setInput] = useState('');
+  const [input, setInput] = useState('asf');
 
-    const clearIcon = input ?
-        <Icon onPress={() => setInput('')} style={createDirectionCardStyle.icon} type={'feather'} name={'x'} onPressIn={() => setInput('')} color={Theme.Light.caption} style={{ alignSelf: 'flex-start' }} />
-        : null;
+  const clearIcon = input ? (
+    <Icon
+      type={'feather'}
+      name={'x'}
+      onPressIn={() => setInput('')}
+      color={Theme.Light.caption}
+      style={createDirectionCardStyle.icon}
+    />
+  ) : null;
 
-    return (
-        <View style={createDirectionCardStyle.container}>
-            <TextInput editable={false} placeholder={'Step 1'} style={createDirectionCardStyle.step} />
-            <TextInput multiline={true} placeholder={'Directions'} value={input} onChangeText={(text: string) => setInput(text)} style={createDirectionCardStyle.input} onSubmitEditing={() => console.log('submitted')} />
-            {clearIcon}
-        </View>
-    );
+  return (
+    <View style={createDirectionCardStyle.container}>
+      <View style={{padding: 0}}>
+        <TextInput
+          style={createDirectionCardStyle.step}
+          placeholder={'Step 1'}
+          editable={false}
+          textAlignVertical={'top'}
+        />
+      </View>
+      <View style={{flex: 1}}>
+        <TextInput
+          placeholder={'asdf'}
+          multiline={true}
+          textAlignVertical={'top'}
+          style={createDirectionCardStyle.input}
+        />
+      </View>
+      {clearIcon}
+    </View>
+  );
 };
 
 const createDirectionCardStyle = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        backgroundColor: Theme.Light.shadow,
-        borderRadius: 5,
-        marginHorizontal: 10,
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        marginTop: 10,
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
-    },
-    step: {
-        ...Typography.Typography.subheader,
-        color: Theme.Light.headline,
-        marginRight: 10,
-        alignSelf: 'flex-start',
-    },
-    input: {
-        flex: 1,
-        ...Typography.Typography.subheader,
-        color: Theme.Light.caption,
-    },
-    icon: {
-        alignSelf: 'flex-start',
-        margin: 10,
-        padding: 10,
-        flex: 1,
-    }
+  container: {
+    flexDirection: 'row',
+    flex: 1,
+    backgroundColor: Theme.Light.shadow,
+    borderRadius: 5,
+    marginHorizontal: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+    alignContent: 'flex-end',
+    marginTop: 10,
+  },
+  step: {
+    ...Typography.Typography.subheader,
+    color: Theme.Light.headline,
+    marginRight: 10,
+  },
+  input: {
+    ...Typography.Typography.subheader,
+    color: Theme.Light.caption,
+  },
+  icon: {
+    flex: 1,
+    marginTop: 8,
+  },
 });
 
 export default CreateDirectionCard;
