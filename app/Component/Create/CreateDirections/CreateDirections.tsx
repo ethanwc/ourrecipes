c26b3c299f6cb6883a2e5c4295326959e414e3db
ethanwc <ethanwc@u.washington.edu>
2020-05-16 18:04:40 -0700

    create card card

--- app/Component/Create/CreateDirections/CreateDirections.tsx
+++ app/Component/Create/CreateDirections/CreateDirections.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { View } from 'react-native';
+import React from 'react';
+import {View} from 'react-native';
 import CreateDirectionCard from '../../../Container/Create/CreateDirectionCard';
+import CreateNewCard from '../../../Container/Create/CreateNewCard';
 
 const CreateDirections = () => {
-    return (
-        <View>
-            <CreateDirectionCard />
-            <CreateDirectionCard />
-            <CreateDirectionCard />
-            <CreateDirectionCard />
-            <CreateDirectionCard />
-
-        </View>
-    )
-}
-
+  return (
+    <View>
+      <CreateDirectionCard />
+      <CreateDirectionCard />
+      <CreateDirectionCard />
+      <CreateDirectionCard />
+      <CreateDirectionCard />
+      <CreateNewCard onPress={() => console.log('add new card')} />
+    </View>
+  );
+};
 
 export default CreateDirections;
---
+++ app/Container/Create/CreateNewCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableHighlight} from 'react-native';
+import {Theme} from '../../assets/styles';
+import {Icon} from 'react-native-elements';
+
+export interface createNewCardProps {
+  onPress: Function;
+}
+
+//card to create a new card
+const CreateNewCard = (props: createNewCardProps) => {
+  const createIcon = (
+    <Icon
+      type={'feather'}
+      name={'plus'}
+      color={Theme.Light.caption}
+      onPress={() => props.onPress()}
+    />
+  );
+
+  return <View style={createNewCardStyle.container}>{createIcon}</View>;
+};
+
+const createNewCardStyle = StyleSheet.create({
+  container: {
+    backgroundColor: Theme.Light.shadow,
+    borderRadius: 5,
+    marginHorizontal: 10,
+    marginTop: 10,
+    padding: 10,
+    justifyContent: 'center',
+  },
+});
+
+export default CreateNewCard;
