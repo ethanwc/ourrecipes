46bba881d72fa4c29fc3b4a5fe26f0e73cf3fc39
ethanwc <ethanwc@u.washington.edu>
2020-06-19 20:25:08 -0700

    redux

---
+++ app/store/index.ts
@@ -0,0 +1,12 @@
+// src/store/index.ts
+//https://redux.js.org/recipes/usage-with-typescript
+
+import { systemReducer } from './system/reducers'
+import { chatReducer } from './chat/reducers'
+
+const rootReducer = combineReducers({
+  system: systemReducer,
+  chat: chatReducer
+})
+
+export type RootState = ReturnType<typeof rootReducer>
---
+++ app/store/system/types.ts
@@ -0,0 +1,17 @@
+// src/store/system/types.ts
+
+export interface SystemState {
+    loggedIn: boolean
+    session: string
+    userName: string
+  }
+
+  // src/store/system/types.ts
+export const UPDATE_SESSION = 'UPDATE_SESSION'
+
+interface UpdateSessionAction {
+  type: typeof UPDATE_SESSION
+  payload: SystemState
+}
+
+export type SystemActionTypes = UpdateSessionAction
