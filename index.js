5fd01665c2fe8c4c1f8d2be11b7a7f2618ca9148
ethanwc <ethanwc@u.washington.edu>
2020-07-09 00:49:02 -0700

    added auth signin via amazon amplify cognito

--- index.js
+++ index.js
@@ -5,5 +5,55 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import {withAuthenticator} from 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
 
-AppRegistry.registerComponent(appName, () => App);
+const awsConfig = {
+  identityPoolId: 'us-west-2:fd59ad3e-1f97-47df-a6b9-0c4c26af498f',
+  region: 'us-west-2',
+  userPoolId: 'us-west-2_hZYGwuQqX',
+  userPoolWebClientId: '34feialh5mvr5tnb15v26vt3l0',
+};
+
+const signUpConfig = {
+  header: 'My Customized Sign Up',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'Name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 2,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 3,
+      type: 'password',
+    },
+    // and other custom attributes
+  ],
+};
+
+Amplify.configure(awsConfig);
+
+AppRegistry.registerComponent(appName, () =>
+  withAuthenticator(App, {
+    signUpConfig: {
+    //   hiddenDefaults: ['phone_number'],
+      signUpFields: [
+        {label: 'Name', key: 'name', required: true, type: 'string'},
+      ],
+    },
+  }),
+);
